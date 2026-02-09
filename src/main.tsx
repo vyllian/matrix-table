b750e2726dfc0ca2c7b5d4fff6ef7609fdfc7cb3
Anastasiia Vylitkova <vylitkovaanastasiia@gmail.com>
2026-02-09 18:52:42 +0200

    changes for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/index.css";
+import App from "@/App.tsx";
 import {MatrixProvider} from "@/core/provider/MatrixProvider.tsx";
 
 createRoot(document.getElementById('root')!).render(
@@ -9,6 +9,5 @@ createRoot(document.getElementById('root')!).render(
       <MatrixProvider>
           <App />
       </MatrixProvider>
-
   </StrictMode>,
 )
